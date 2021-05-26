754dfbb728932b13b6494d59b30a1cef3806e496
lucasletro <83148640+lucasletro@users.noreply.github.com>
2021-05-26 10:30:19 -0300

    chat online flutter

---
+++ android/app/src/main/kotlin/br/com/letro/chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.letro.chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+
+  Firestore.instance.collection("mensagens").document("ILMLyArWGSKD64w4AAdc").collection("arquivos").document().setData({
+    "arqname": "foto.png"
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
+
